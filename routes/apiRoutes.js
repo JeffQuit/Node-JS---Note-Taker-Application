3605bbee48f7c935b96abb9d2b7c5cb4cce6e87a
Jeff Q <Jeffquittman@gmail.com>
2020-06-20 09:30:47 -0700

    testing logging of req.body functioanlity and got the new note format to post to the console

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,4 +11,13 @@ module.exports = function (app) {
 	app.get('/api/notes', function (req, res) {
 		return res.json(jsonData);
 	});
+
+	//Functionality to add new note using post method
+	app.post('/api/notes', function (req, res) {
+		let addNote = req.body;
+
+		console.log(addNote);
+
+		res.json(addNote);
+	});
 };
